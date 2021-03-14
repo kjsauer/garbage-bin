96a1b3a3e66ecb619e1c1c4093ddbb954e048dcf
Kenneth J. Sauer <31974927+kjsauer@users.noreply.github.com>
2021-03-14 13:36:54 -0700

    Create simple_for_loop.py
    add content

---
+++ garbage-bin/simple_for_loop.py
@@ -0,0 +1,7 @@
+for ii in range(3):
+    print("Hello! ii is set to", ii)
+# Backslash maintains the character
+for ii in range(3):
+    print("It\'s totally set to ", ii, "!")
+for ii in range(3):
+    print("No really it\'s totally set to " + str(ii), "!!!")
