d86b785eaf3465f9f98499c3c1eccca2e760e444
Kenneth J. Sauer <31974927+kjsauer@users.noreply.github.com>
2021-04-10 14:58:12 -0700

    Create play_w_strings.py
    add content

---
+++ garbage-bin/python/play_w_strings.py
@@ -0,0 +1,14 @@
+# Make strings in Python
+A = 'Cat'
+B = 'Dog'
+C = '3'
+# Method 1
+make_string_1 = '%s\n%s\n%s'%(A,B,C)
+# Print string result of Method 1
+print(make_string_1)
+# Method 2
+make_string_2= '''%s
+%s
+%s'''%(A,B,C)
+# Print string result of Method 2
+print(make_string_2)
